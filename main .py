7089f498ea49418ea3ef0bce0c08a6e5ef48ddfb
Akshatamish <amishkumar1103@gmail.com>
2025-09-17 20:14:45 +0530

    Delete main .py

--- main .py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-df=pd.read_csv("data.txt") # Load dataset
-print(df)
-df.to_excel("data.xlsx",index=False) #save dataset into excel file
-# print("File converted succesfully")
-print(df.head())
